27b5104d561bd5b5331db44a174a01fc88ad1f69
Stag Tack PC <61768347+vickycxc@users.noreply.github.com>
2020-06-20 08:39:15 +0800

    Bismillah pindah ke Flutter

--- app/src/main/java/com/alfatih/goremas/MainActivity.kt
+++ app/src/main/java/com/alfatih/goremas/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.alfatih.goremas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        btnLogin.setOnClickListener {
+            val username = inputUsername.text.toString()
+            val password = inputPassword.text.toString()
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please Insert Username and Password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            } else {
+                val intent = Intent(this, Dashboard::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
